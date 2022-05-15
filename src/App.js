10980f459c3223b8b4b52ad8465741dcf9e99634
ShrutiLakhani <82932172+ShrutiLakhani@users.noreply.github.com>
2022-05-15 23:44:19 +0530

    Folder Structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Navbar } from "./Components/index.js";
+import { Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
+}
+export default App;
