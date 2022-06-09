938b512b60e61436c889a06db3a53800fd8d257e
ShrutiLakhani <82932172+ShrutiLakhani@users.noreply.github.com>
2022-06-09 18:46:42 +0530

    Refactoring

--- src/Pages/Pages/HomePage/HomePage.jsx
+++ src/Pages/Pages/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import "./HomePage.css";
 import { HeroSection } from "../../../Components/index";
-// import { useFilter } from "../../context/filter-context";
 
 export function HomePage() {
   return (
